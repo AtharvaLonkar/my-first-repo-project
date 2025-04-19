49b88d0ff4b21e4a63d3b708d83188af3d0e18e6
atharva <atharva@inspecity.com>
2025-04-19 13:06:14 +0530

    ADD code update

---
+++ main.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+int main()
+{
+    int i=0,j=0;
+    // int sum;
+    printf("Enter a number : ");
+    scanf("%d\n",&i);
+    printf("Enter a number : ");
+    scanf("%d\n",&j);
+    printf("Sum %d\n",i+j);
+    return 0;
+}
